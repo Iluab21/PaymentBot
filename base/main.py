aee40335292b730641cdbf833f176f0c52012a1b
kompuktor <ilua5236@gmail.com>
2022-07-04 17:33:37 +0300

    Первая рабочая версия

--- base/main.py
+++ base/main.py
@@ -1,104 +1,250 @@
+import os
 from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon import TelegramClient
 from telethon import events
 from telethon.tl.custom import Button
+import telethon
 import asyncio
+from telethon.sessions import StringSession
 import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 
-api_id = 000000
-api_hash = '00000000000000000000000000'
-bot_token = "000000000000000000000000"
-adminid = 000000000
-channel = 0000000000000
+api_id = int(os.getenv('API_ID'))
+api_hash = os.getenv('API_HASH')
+token = os.getenv('TOKEN')
+admin_id = int(os.getenv('ADMIN_ID'))
+open_channel = int(os.getenv('FREE_CHANNEL'))
+channel = int(os.getenv('CHANNEL'))
+session = os.getenv('SESSION')
+client = TelegramClient(StringSession(session), api_id, api_hash)
+bot = TelegramClient('bot', api_id, api_hash)
+# Пробуем подгрузить инфу с прошлых запусков
+try:
+    with open('back.txt', 'r') as f:
+        d = eval(f.read())
+except (EOFError, OSError, SyntaxError):
+    d = {}
+# Текстовый логгер ошибок
+with open('logging.txt', 'w') as l:
+    l.write('Exceptions log:')
+m = 0
+message = {}
 
-client = TelegramClient('anon', api_id, api_hash)
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+async def invite(users_to_add):
+    # Добавление пользователя в чат
+
+    async with TelegramClient(StringSession(session), api_id, api_hash) as client:
+        try:
+            await client.get_participants(open_channel)
+            await client(InviteToChannelRequest(channel=channel, users=[users_to_add]))
+        except ValueError:
+            await bot.send_message(admin_id, 'Косяк ' + str(users_to_add))
+            keyboard = [Button.inline('Назад', b'main')]
+            await bot.send_message(users_to_add, 'Для того чтобы попасть в закрытый канал, вы должны быть подписчиком канала t.me/ALGtrader', buttons= keyboard)
+        except:
+            await bot.send_message(admin_id, 'Неожиданный косяк с ' + str(users_to_add))
 
 
-async def invite(channel, users_to_add):
-    await client(InviteToChannelRequest(
-        channel,
-        [users_to_add]
-    ))
+async def remove(user):
+    # Удаление пользователя из чата
+    async with TelegramClient(StringSession(session), api_id, api_hash) as client:
+        try:
+            await client.get_participants(channel)
+            await client.kick_participant(channel, user)
+        except:
+            await bot.send_message(admin_id, 'Неожиданный косяк с ' + str(user))
 
-
-async def remove(channel, user):
-    async with TelegramClient('anon', api_id, api_hash) as client:
-        await client.kick_participant(channel, user)
-
-
-# asyncio.run(invite(channel, users_to_add))
-# asyncio.run(remove(channel, user))
 
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def starthandler(event):
-    global d
-    # Ответ на первый запуск бота, добавление пользователя в базу
-    keyboard = [Button.inline('Оплата', b'pay')], [Button.inline('Сколько дней подписки осталось?', b'check')], [
-        Button.inline('Всё оплачено, добавьте меня', b'reinvite')]
+    # Ответ на первый запуск бота. Добавление пользователя в базу, если его там нет.
+    global m
     entity = await bot.get_entity(event.chat_id)
-    await bot.send_message(event.chat_id, 'Привет! Я бот для оплаты подписки. \nВыбери, что ты хочешь сделать.',
-                           buttons=keyboard)
     if event.chat_id in d:
-        print(d)
+        keyboard = [Button.inline('Оплата', b'pay')], [Button.inline('Сколько дней подписки осталось?', b'check')], [
+            Button.inline('Всё оплачено, добавьте меня', b'reinvite')]
+        try:
+            await bot.delete_messages(event.chat_id, message[event.chat_id])
+        except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+            pass
+        message[event.chat_id] = await bot.send_message(event.chat_id, 'Выбери, что ты хочешь сделать.',
+                                   buttons=keyboard)
     else:
         d[event.chat_id] = [5, entity.id, entity.first_name, entity.last_name, entity.username, 0]
-        print(d)
+        keyboard = [Button.inline('Оплата', b'pay')], [Button.inline('Сколько дней подписки осталось?', b'check')], [
+            Button.inline('Всё оплачено, добавьте меня', b'reinvite')]
+        try:
+            await bot.delete_messages(event.chat_id, message[event.chat_id])
+        except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+            pass
+        message[event.chat_id] = await bot.send_message(event.chat_id,
+                                   'Привет! Я бот-менеджер для нашего торгового бота. Сейчас для новичков есть ' +
+                                   str(d[event.chat_id][
+                                       0]) + 'бесплатных дней для ознакомления, мы уже добавляем тебя в чат) '
+                                             '\nВыбери, что ты хочешь сделать.',
+                                   buttons=keyboard)
+        isparticipant = await asyncio.ensure_future(search(event.chat_id))
+        if isparticipant:
+            try:
+                await bot.delete_messages(event.chat_id, message[event.chat_id])
+            except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                pass
+            keyboard = [Button.inline('Назад', b'main')]
+            message[event.chat_id] = await bot.send_message(event.chat_id, 'Вы уже в чате', buttons=keyboard)
+        else:
+            try:
+                await bot.delete_messages(event.chat_id, message[event.chat_id])
+            except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                pass
+            keyboard = [Button.inline('Оплата', b'pay')], [
+                Button.inline('Сколько дней подписки осталось?', b'check')], [
+                           Button.inline('Всё оплачено, добавьте меня', b'reinvite')]
+            message[event.chat_id] = await bot.send_message(event.chat_id, 'Привет! Я бот-менеджер для нашего торгового бота. Сейчас для'
+                                                      ' новичков есть ' +
+                                   str(d[event.chat_id][
+                                       0]) + 'бесплатных дней для ознакомления, мы уже добавляем тебя в чат) '
+                                             '\nВыбери, что ты хочешь сделать.', buttons=keyboard)
+            await asyncio.ensure_future(invite(event.chat_id))
 
 
 @bot.on(events.CallbackQuery(data=b'check'))
 async def checkhandler(event):
+    global m
     # Проверка оставшейся подписки
     try:
-        await bot.send_message(event.chat_id, 'Осталось ' + str(d[event.chat_id][0]) + ' дней')
+        try:
+            await bot.delete_messages(event.chat_id, message[event.chat_id])
+        except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+            pass
+        keyboard = [Button.inline('Назад', b'main')]
+        message[event.chat_id] = await bot.send_message(event.chat_id, 'Осталось ' + str(d[event.chat_id][0]) + ' дней', buttons=keyboard)
     except Exception as err:
         logging.error(err, exc_info=True)
 
 
 @bot.on(events.CallbackQuery(data=b'reinvite'))
 async def inviting(event):
+    global m
     # Приглашение пользователя, если у него активна подписка, и его нет в чате.
+    try:
+        await bot.delete_messages(event.chat_id, message[event.chat_id])
+    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+        pass
     if d[event.chat_id][0] > 0:
         isparticipant = await asyncio.ensure_future(search(event.chat_id))
         if isparticipant:
-            await bot.send_message(event.chat_id, 'Вы уже в чате')
+            keyboard = [Button.inline('Назад', b'main')]
+            message[event.chat_id] = await bot.send_message(event.chat_id, 'Вы уже в чате', buttons=keyboard)
         else:
-            await bot.send_message(event.chat_id, 'Добавляю')
-            # invite(event.chat_id)
+            keyboard = [Button.inline('Назад', b'main')]
+            message[event.chat_id] = await bot.send_message(event.chat_id, 'Добавляю', buttons=keyboard)
+            await asyncio.ensure_future(invite(event.chat_id))
     else:
-        await bot.send_message(event.chat_id, 'Ваша подписка неактивна')
+        keyboard = [Button.inline('Назад', b'main')]
+        message[event.chat_id] = await bot.send_message(event.chat_id, 'Ваша подписка неактивна', buttons=keyboard)
+
 
 @bot.on(events.CallbackQuery(data=b'pay'))
 async def payhandler(event):
+    global m
     # Функция выбора количества месяцев для оплаты
-    keyboard = [Button.inline('1', b'month_1')], [Button.inline('2', b'month_2')], [Button.inline('3', b'month_3')],\
-        [Button.inline('6', b'month_6')], [Button.inline('12', b'month_12')]
-    await bot.send_message(event.chat_id, 'Выберите количество месяцев на оплату',
-                           buttons=keyboard)
+    try:
+        await bot.delete_messages(event.chat_id, message[event.chat_id])
+    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+        pass
+    keyboard = [Button.inline('1', b'month_1'), Button.inline('2', b'month_2')], [Button.inline('3', b'month_3'),
+                                                                                  Button.inline('6', b'month_6')], [
+                   Button.inline('12', b'month_12'), Button.inline('Назад', b'main')]
+    message[event.chat_id] = await bot.send_message(event.chat_id, 'Выберите количество месяцев на оплату',
+                               buttons=keyboard)
+
 
 @bot.on(events.CallbackQuery(pattern='(?i)month.+'))
 async def monthhandler(event):
-    await bot.send_message(event.chat_id, 'Ссылка для оплаты на ' + str(int(str(event.data)[8:-1]) * 15) + '$')
-    print(d[event.chat_id])
+    global m
+    # Подтверждение платежа
+    try:
+        await bot.delete_messages(event.chat_id, message[event.chat_id])
+    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+        pass
+    keyboard = [Button.inline('Оплачено', b'payed')], [Button.inline('Назад', b'pay')]
+    message[event.chat_id] = await bot.send_message(event.chat_id, 'Для оплаты нужно отправить ' + str(int(str(event.data)[8:-1]) * 15 + 5) + " USDT на Binance id 257063459.\nВ заметке к платежу указать имя и фамилию или ник, как вы записаны в Telegram, чтобы вас можно было идентифицировать",
+                               buttons=keyboard)
     d[event.chat_id][5] = int(str(event.data)[8:-1]) * 31
+
+
+@bot.on(events.CallbackQuery(data=b'payed'))
+async def payed(event):
+    global m
+    # Функция перевода на подтверждение платежа админом
+    try:
+        await bot.delete_messages(event.chat_id, message[event.chat_id])
+    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+        pass
+    keyboard = [Button.inline('В главное меню', b'main')]
+    message[event.chat_id] = await bot.send_message(event.chat_id,
+                               'Сейчас мы проверим оплату и, если всё ок, в течение суток добавим вас в канал',
+                               buttons=keyboard)
     await asyncio.ensure_future(confirm(event.chat_id))
 
+
+@bot.on(events.CallbackQuery(data=b'main'))
+async def payeds(event):
+    # Возвращение в главное меню
+    global m
+    await asyncio.ensure_future(starthandler(event))
+
+
 async def confirm(userid):
-    keyboard = [Button.inline('Подтвердить', b'confirm')], [Button.inline('Не подтверждать', b'cancel')]
-    m = await bot.send_message(adminid, str(d[userid]), buttons=keyboard)
+    # Функция подтверждения оплаты
+    global m
+    keyboard = [Button.inline('Подтвердить', data=str(userid))], [
+        Button.inline('Не подтверждать', data='cancel' + str(userid))]
+    admin_message = await bot.send_message(admin_id, str(d[userid]), buttons=keyboard)
+
+
     @bot.on(events.CallbackQuery())
-    async def payhandler(event):
-        if event.chat_id == adminid:
-            if event.data == b'confirm':
-                await bot.send_message(userid, 'Добавляю в канал')
-                await bot.delete_messages(adminid, m)
+    async def pay(event):
+        global m
+        if event.chat_id == admin_id:
+            if event.data == str(userid).encode():
+                try:
+                    await bot.delete_messages(userid, message[event.chat_id])
+                except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                    pass
+                message[event.chat_id] = await bot.send_message(userid, 'Подписку подтвердили, если вас нет в канале, добавим)')
+                try:
+                    await bot.delete_messages(admin_id, admin_message)
+                except (telethon.errors.MessageDeleteForbiddenError, KeyError) :
+                    pass
                 d[userid][0] += d[userid][5]
-                # await asyncio.ensure_future(inviting(userid))
-            if event.data == b'cancel':
-                await bot.send_message(userid, 'Платёж не подтвеждён')
-                await bot.delete_messages(adminid, m)
+                isparticipant = await asyncio.ensure_future(search(event.chat_id))
+                if isparticipant:
+                    try:
+                        await bot.delete_messages(userid, message[event.chat_id])
+                    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                        pass
+                    keyboard = [Button.inline('Назад', b'main')]
+                    message[event.chat_id] = await bot.send_message(userid, 'Вы уже в чате', buttons=keyboard)
+                else:
+                    try:
+                        await bot.delete_messages(userid, message[event.chat_id])
+                    except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                        pass
+                    keyboard = [Button.inline('Назад', b'main')]
+                    message[event.chat_id] = await bot.send_message(userid, 'Добавляю вас в чат', buttons=keyboard)
+                    await asyncio.ensure_future(invite(userid))
+            if event.data == b'cancel' + str(userid).encode():
+                keyboard = [Button.inline('Назад', b'main')]
+                message[event.chat_id] = await bot.send_message(userid, 'Платёж не подтвеждён', buttons=keyboard)
+                try:
+                    await bot.delete_messages(admin_id, admin_message)
+                except (telethon.errors.MessageDeleteForbiddenError, KeyError):
+                    pass
                 return False
+
 
 async def search(userid):
     # Функция поиска по пользователям канала
@@ -115,92 +261,43 @@ async def search(userid):
         return False
 
 
-d = {}
-bot.start()
-bot.run_until_disconnected()
+async def main():
+    # Старт бота, старт таймера
+    await bot.start(bot_token=token)
+    asyncio.ensure_future(timer())
+    try:
+        await bot.run_until_disconnected()
+    except Exception as err:
+        logging.error(err, exc_info=True)
+        with open('logging.txt', 'a') as l:
+            l.write('\n' + str(err))
+    finally:
+        await bot.disconnect()
 
-# def paying():
-#     @bot.message_handler(commands=["start"])
-#     def start(m):
-#         global userid, d
-#         key = types.InlineKeyboardMarkup()
-#         key.add(types.InlineKeyboardButton(text='Оплата', callback_data="butt1"))
-#         key.add(types.InlineKeyboardButton(text='Количество дней до окончания подписки', callback_data="butt2"))
-#         bot.send_message(m.chat.id, 'Добро пожаловать, если уже оплатили, нажмите /newlink', reply_markup=key)
-#         userid = m.chat.id
-#         if userid in d:
-#             print(d)
-#         else:
-#             d[userid] = [0, m.from_user.first_name, m.from_user.last_name, m.from_user.username, day]
-#             print(d)
-#
-#     @bot.message_handler(commands=["newlink"])
-#     def newlink(m):
-#         global userid, d
-#         if d[userid][0] > 0:
-#             bot.send_message(userid, "Добавляю вас в канал)")
-#             # adding()
-#         if d[userid][0] == 0:
-#             bot.send_message(userid, "Подписка неактивна")
-#
-#     @bot.callback_query_handler(func=lambda call: True)
-#     def inline(c):
-#         global userid, d, add
-#         if c.data == 'butt1':
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='1', callback_data="b1"))
-#             key.add(types.InlineKeyboardButton(text='2', callback_data="b2"))
-#             key.add(types.InlineKeyboardButton(text='3', callback_data="b3"))
-#             key.add(types.InlineKeyboardButton(text='6', callback_data="b6"))
-#             key.add(types.InlineKeyboardButton(text='12', callback_data="b12"))
-#             bot.send_message(userid, 'Выберите количество месяцев', reply_markup=key)
-#
-#         if c.data == 'butt2':
-#             if d[userid][0] > 0:
-#                 bot.send_message(userid, 'Осталось ' + str(d[userid][0]) + ' дней')
-#             if d[userid][0] == 0:
-#                 bot.send_message(userid, 'Подписка неактивна')
-#
-#         if c.data == 'b1':
-#             d[userid][4] = 31
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='Оплачено', callback_data="payed"))
-#             bot.send_message(userid, 'Ссылка на оплату 1 мес', reply_markup=key)
-#         if c.data == 'b2':
-#             d[userid][4] = 62
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='Оплачено', callback_data="payed"))
-#             bot.send_message(userid, 'Ссылка на оплату 2 мес', reply_markup=key)
-#         if c.data == 'b3':
-#             d[userid][4] = 93
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='Оплачено', callback_data="payed"))
-#             bot.send_message(userid, 'Ссылка на оплату 3 мес', reply_markup=key)
-#         if c.data == 'b6':
-#             d[userid][4] = 186
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='Оплачено', callback_data="payed"))
-#             bot.send_message(userid, 'Ссылка на оплату 6 мес', reply_markup=key)
-#         if c.data == 'b12':
-#             d[userid][4] = 372
-#             key = types.InlineKeyboardMarkup()
-#             key.add(types.InlineKeyboardButton(text='Оплачено', callback_data="payed"))
-#             bot.send_message(userid, 'Ссылка на оплату 12 мес', reply_markup=key)
-#
-#         if c.data == 'payed':
-#             confirm()
-#             bot.send_message(userid, 'Если всё оплачено, в течение суток добавим вас в канал)')
-#
-#         if len(c.data) > 7:
-#             d[int(c.data[4:])][0] = d[int(c.data[4:])][4]
-#             bot.send_message(c.data[4:], "Добавляю вас в канал)")
-#             add = c.data[4:]
-#
-#         if c.data == 'buttn':
-#             print(userid)
-#             bot.send_message(userid, "Оплату не подтвердили")
-#
-#
-#     @bot.message_handler(content_types='text')
-#     def message_reply(message):
-#         pass
+
+async def timer():
+    global d
+    # Таймер отсчёта дней
+    while True:
+        await asyncio.sleep(86400)
+        users = await bot.get_participants(channel)
+        for i in users:
+            try:
+                if d[i.id][0] == 0:
+                    asyncio.ensure_future(remove(i.id))
+                if d[i.id][0] > 0:
+                    d[i.id][0] -= 1
+                else:
+                    pass
+            except KeyError:
+                pass
+        # Чтобы информация сохранялась после перезапуска
+        with open('back.txt', 'w') as f:
+            f.write(str(d))
+
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
